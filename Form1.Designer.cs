dea63fe70d85740aa30d98cdf31320d69546ff59
AhmdeAbid <ahmed.abid@triazur.com>
2022-10-21 17:58:37 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Sandbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.button1.Image = global::Sandbox.Properties.Resources.Gaming_5000x3125;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TEST_BUTTON";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1410, 785);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "My Chromium Web Browser";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace Sandbox
+{
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser chromeBrowser;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // At the initialization, strat chromium
+            InitializeChromium();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+
+            // Initialize Cef with provided settings
+            Cef.Initialize(settings);
+
+            // Create a browser component and provide a start web url
+            chromeBrowser = new ChromiumWebBrowser("http://www.google.com");
+
+            // Add the browser to the form
+            this.Controls.Add(chromeBrowser);
+
+            // Make the browser fill to the form
+            chromeBrowser.Dock = DockStyle.Fill;
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
